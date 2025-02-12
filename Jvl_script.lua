7a2f1fc21141e56d631843f68ccc94945a112374
cesluk <ceslukm@gmail.com>
2025-02-12 20:48:18 +0300

    Update Jvl_script.lua

--- Jvl_script.lua
+++ Jvl_script.lua
@@ -8,7 +8,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Aqua",
+    Theme = "Amethyst",
     MinimizeKey = Enum.KeyCode.RightControl -- Used when theres no MinimizeKeybind
 })
 
